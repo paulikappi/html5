02346a08b842b5acd581026f2f5f585a2979b8d3
paulikappi <pauli.kappi@gmail.com>
2020-06-03 16:32:09 +0300

    started turunsanomat 1905

---
+++ turunsanomat.js
@@ -0,0 +1,7 @@
+let width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+let height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
